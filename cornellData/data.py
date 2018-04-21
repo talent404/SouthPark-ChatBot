aefd24aef2a42182d91116a9e9a051054c3655fc
Harshavardhan V <harsha.v@ada.iiit.ac.in>
2018-04-21 19:02:30 +0530

    some data changes

--- cornellData/data.py
+++ cornellData/data.py
@@ -32,29 +32,32 @@ for conv in convs:
 
 def clean_text(text):
     '''Clean text by removing unnecessary characters and altering the format of words.'''
-
     text = text.lower()
     
+    text = re.sub(r"\n", "",  text)
+    text = re.sub(r"[-()]", "", text)
+    text = re.sub(r"\.", " .", text)
+    text = re.sub(r"\!", " !", text)
+    text = re.sub(r"\?", " ?", text)
+    text = re.sub(r"\,", " ,", text)
     text = re.sub(r"i'm", "i am", text)
     text = re.sub(r"he's", "he is", text)
     text = re.sub(r"she's", "she is", text)
     text = re.sub(r"it's", "it is", text)
     text = re.sub(r"that's", "that is", text)
     text = re.sub(r"what's", "that is", text)
-    text = re.sub(r"where's", "where is", text)
-    text = re.sub(r"how's", "how is", text)
     text = re.sub(r"\'ll", " will", text)
-    text = re.sub(r"\'ve", " have", text)
-    text = re.sub(r"\'re", " are", text)
-    text = re.sub(r"\'d", " would", text)
     text = re.sub(r"\'re", " are", text)
     text = re.sub(r"won't", "will not", text)
     text = re.sub(r"can't", "cannot", text)
     text = re.sub(r"n't", " not", text)
     text = re.sub(r"n'", "ng", text)
-    text = re.sub(r"'bout", "about", text)
-    text = re.sub(r"'til", "until", text)
-    text = re.sub(r"[-()\"#/@;:<>{}`+=~|.!?,]", "", text)
+    text = re.sub(r"ohh", "oh", text)
+    text = re.sub(r"ohhh", "oh", text)
+    text = re.sub(r"ohhhh", "oh", text)
+    text = re.sub(r"ohhhhh", "oh", text)
+    text = re.sub(r"ohhhhhh", "oh", text)
+    text = re.sub(r"ahh", "ah", text)
     
     return text
 
@@ -65,8 +68,7 @@ for i in range(len(answers)):
     answers[i] = clean_text(answers[i])
 
 
-for i in range(10):
-    print questions[i]
-    print answers[i]
-    print 
-        
+for i in range(len(questions)):
+    print(questions[i]+'\t'+answers[i])
+#    print 
+
